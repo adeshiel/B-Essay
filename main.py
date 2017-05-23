bc60c87c7141cdcc02d2c4d61e3410730cba7bf8
Ashley Deshields <adeshiel@bu.edu>
2017-05-23 17:57:00 -0400

    Created function to find the longest synonym and appends it, to be tested

---
+++ main.py
@@ -0,0 +1,33 @@
+# Main functions
+#Python 2.7
+import unirest
+
+class makeNewEssay(object):
+    """docstring for ."""
+    def __init__(self, essay):
+        super(, self).__init__()
+        self.essay = essay
+        newEssay = ""
+
+    def pickLongestSynonym():
+        """ goes through each word in the essay to find a longer synonym and appends
+        it to the new Essay """
+        for x in len(self.essay):
+            response = unirest.get("https://wordsapiv1.p.mashape.com/words/{x}/synonyms",
+                headers={
+                    "X-Mashape-Key": "o4BB4YatyVmshNlvtMFsZNXCDPcmp1u8RNQjsnb2RscDXVMK0f",
+                    "Accept": "application/json"
+                }
+            )
+            if(len(response.items()) != 0 && len(response.items()[1]) != 0):
+                newmeanings = response.items()[1][1:]
+                if(len(newmeanings) > 1):
+                    longest = newmeanings[0]
+                    for y in newmeanings:
+                        if(len(newmeanings[y]) >= longest):
+                            longest = newmeanings[y]
+                    newEssay = newEssay + longest + " "
+                else:
+                    newEssay = newEssay + newmeanings[1] + " "
+            else:
+                newEssay = newEssay + x + " "
