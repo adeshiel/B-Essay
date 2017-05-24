1e552c1f520a7e90b037ecde365d8d86299400f5
Ashley Deshields <ashley.deshields.2016@gmail.com>
2017-05-23 20:19:35 -0400

    Need to translate JSON to Python, but API is causing trouble

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 # Main functions
 #Python 2.7
+import json
 import unirest
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
 class makeNewEssay(object):
     """makes a really silly essay"""
     def __init__(self, essay):
         self.essay = essay
         newEssay = ""
 
-    def pickLongestSynonym():
+    def pickLongestSynonym(self):
         """ goes through each word in the essay to find a longer synonym and appends
         it to the new Essay """
-        for x in len(self.essay):
-            response = unirest.get("https://wordsapiv1.p.mashape.com/words/{x}/synonyms",
+        for x in self.essay:
+            word = x
+            array = unirest.get("https://wordsapiv1.p.mashape.com/words/{word}/synonyms",
                 headers={
                     "X-Mashape-Key": "o4BB4YatyVmshNlvtMFsZNXCDPcmp1u8RNQjsnb2RscDXVMK0f",
                     "Accept": "application/json"
                 }
             )
+            response = json.loads(array)
+            print(response)
             if(len(response.items()) != 0 & len(response.items()[1]) != 0):
                 newmeanings = response.items()[1][1:]
                 if(len(newmeanings) > 1):
@@ -30,3 +37,15 @@ class makeNewEssay(object):
                     newEssay = newEssay + newmeanings[1] + " "
             else:
                 newEssay = newEssay + x + " "
+
+        return newEssay
+
+#response = unirest.get("https://wordsapiv1.p.mashape.com/words/{x}/synonyms",
+#    headers={
+#        "X-Mashape-Key": "o4BB4YatyVmshNlvtMFsZNXCDPcmp1u8RNQjsnb2RscDXVMK0f",
+#        "Accept": "application/json"
+#    }
+#)
+
+test = makeNewEssay("Help me.")
+print(test.pickLongestSynonym())
