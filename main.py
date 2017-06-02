74ccc42ef7dde854ab0b986f5c74a6806e8927b5
Ashley Deshields <ashley.deshields.2016@gmail.com>
2017-06-02 13:33:35 -0400

    Function works, needs some tweaking for punctuation

--- main.py
+++ main.py
@@ -10,34 +10,37 @@ app = Flask(__name__)
 class makeNewEssay(object):
     """makes a really silly essay"""
     def __init__(self, essay):
-        self.essay = essay
-        newEssay = ""
+        self.essay = essay.split()
+        new_essay = ""
+        self.new_essay = new_essay
 
     def pickLongestSynonym(self):
         """ goes through each word in the essay to find a longer synonym and appends
         it to the new Essay """
         for x in self.essay:
-            response = unirest.get("https://wordsapiv1.p.mashape.com/words/" +str(x) +"/synonyms",
+            ret = unirest.get("https://wordsapiv1.p.mashape.com/words/" +str(x) +"/synonyms",
                 headers={
                     "X-Mashape-Key": "o4BB4YatyVmshNlvtMFsZNXCDPcmp1u8RNQjsnb2RscDXVMK0f",
                     "Accept": "application/json"
                 }
             )
-            new_essay = self.essay
-            if(len(response["synonyms"]) != None): #& len(response.items()[1]) != 0):
-                new_word = response["synonyms"][0]
-                if(len(response["synonyms"] > 1):
+            response = json.loads(ret._raw_body)
+            #synonym = json.loads(response)
+            #return synonym
+            if(len(response['synonyms']) != 0): #& len(response.items()[1]) != 0):
+                new_word = response['synonyms'][0]
+                if(len(response['synonyms']) > 1):
                     longest = new_word
-                    for y in response["synonyms"]:
+                    for y in response['synonyms']:
                         if(len(y) >= longest):
                             longest = y
-                    new_essay = new_essay + longest + " "
+                    self.new_essay = self.new_essay + longest + " "
                 else:
-                    new_essay = new_essay + new_word + " "
+                    self.new_essay = self.new_essay + new_word + " "
             else:
-                new_essay = new_essay + x + " "
+                self.new_essay = self.new_essay + x + " "
 
-        return new_essay
+        print(self.new_essay)
 
     def testingKey(self):
         word = self.essay
@@ -57,6 +60,6 @@ class makeNewEssay(object):
 #    }
 #)
 
-test = makeNewEssay("Help")
-#print(test.pickLongestSynonym())
-print(test.testingKey())
+test = makeNewEssay("Please help my dear Aunt Sally")
+print(test.pickLongestSynonym())
+#print(test.testingKey())
